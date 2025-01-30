7e09679caa558d113ae0468b27f86b00e204ac1d
tpokora <pokora.tomek@gmail.com>
2025-01-30 21:40:47 +0100

    Small change to dice rolls form handling

--- app/dice_rolls/dice_roll_routes.py
+++ app/dice_rolls/dice_roll_routes.py
@@ -12,18 +12,22 @@ logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(
 
 
 @bp.route('/dice_roll', methods=['GET', 'POST'])
-def dice_rolls():
+def dice_rolls() -> str:
     form = DiceRollForm()
     if request.method == 'POST':
-        if form.validate_on_submit():
-            roll_results = DiceRoll(f'{form.dice_number.data}d{form.dice_size.data}').roll()
-            return render_template(ROLLS_DICE_ROLL_HTML_TEMPLATE, form=form, roll_results=roll_results)
-        else:
-            if form.dice_number.errors:
-                flash(form.dice_number.errors[0])
-            if form.dice_size.errors:
-                flash(form.dice_size.errors[0])
-            return render_template(ROLLS_DICE_ROLL_HTML_TEMPLATE, form=form)
+        return __handle_form(form)
 
     return render_template(ROLLS_DICE_ROLL_HTML_TEMPLATE, form=form)
+
+
+def __handle_form(form) -> str:
+    if form.validate_on_submit():
+        roll_results = DiceRoll(f'{form.dice_number.data}d{form.dice_size.data}').roll()
+        return render_template(ROLLS_DICE_ROLL_HTML_TEMPLATE, form=form, roll_results=roll_results)
+    else:
+        if form.dice_number.errors:
+            flash(form.dice_number.errors[0])
+        if form.dice_size.errors:
+            flash(form.dice_size.errors[0])
+        return render_template(ROLLS_DICE_ROLL_HTML_TEMPLATE, form=form)
 
