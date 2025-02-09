7a32e4d89f5e868ecb4b8d46c5d0fc1c13cb2037
tpokora <pokora.tomek@gmail.com>
2025-02-09 19:09:05 +0100

    Basic crusade forces view

--- app/crusade_force/crusade_views_routes.py
+++ app/crusade_force/crusade_views_routes.py
@@ -4,11 +4,11 @@ from flask import render_template
 
 from app.crusade_force import bp
 
-ROLLS_DICE_ROLL_HTML_TEMPLATE = 'crusade_force/crusade_force.html'
+CRUSADES_HTML_TEMPLATE = 'crusade_force/crusade_force.html'
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s')
 
 
 @bp.route('/crusades', methods=['GET'])
 def crusade_forces_home() -> str:
-    return render_template(ROLLS_DICE_ROLL_HTML_TEMPLATE)
+    return render_template(CRUSADES_HTML_TEMPLATE)
