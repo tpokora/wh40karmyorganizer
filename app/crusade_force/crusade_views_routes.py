15aa4a2c1e8e0397c38a40c1d22a5b3ca700b29f
tpokora <pokora.tomek@gmail.com>
2025-02-10 21:49:45 +0100

    Crusade force list changed to class view

--- app/crusade_force/crusade_views_routes.py
+++ app/crusade_force/crusade_views_routes.py
@@ -1,6 +1,7 @@
 import logging
 
 from flask import render_template
+from flask.views import MethodView
 
 from app.crusade_force import bp
 from app.crusade_force.crusade import CrusadeService
@@ -9,11 +10,14 @@ CRUSADES_HTML_TEMPLATE = 'crusade_force/crusade_force.html'
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s')
 
-crusade_service = CrusadeService()
+
+class CrusadeForcesView(MethodView):
+    def __init__(self):
+        self.crusade_service = CrusadeService()
+
+    def get(self) -> str:
+        logging.info("Displaying all crusade forces...")
+        return render_template(CRUSADES_HTML_TEMPLATE, crusade_forces=self.crusade_service.get_all())
 
 
-@bp.route('/crusades', methods=['GET'])
-def crusade_forces_home() -> str:
-    logging.info("Displaying all crusade forces...")
-    crusade_forces = crusade_service.get_all()
-    return render_template(CRUSADES_HTML_TEMPLATE, crusade_forces=crusade_forces)
+bp.add_url_rule('/crusades', view_func=CrusadeForcesView.as_view('crusade_forces_home'))
